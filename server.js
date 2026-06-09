645c6c3c4c2dfdcd8578b4d8741b61c0e60b3e20
dhanushiya-A <dhanushiya8207@gmail.com>
2026-06-09 08:25:34 +0530

    fix static file route

--- server.js
+++ server.js
@@ -1,58 +1,3 @@
-// require('dotenv').config();
-// const express = require('express');
-// const path = require('path');
-// const cors = require('cors');
-// const { supabaseAdmin, supabaseAnon } = require('./utils/supabaseClient');
-// const pollRoutes = require('./routes/pollRoutes');
-// const questionRoutes = require('./routes/questionRoutes');
-// const authRoutes = require('./routes/authRoutes');
-// const aiRoutes = require('./routes/aiRoutes');
-
-// const app = express();
-// const PORT = process.env.PORT || 5000;
-
-// app.use((req, res, next) => {
-//   console.log(`Request: ${req.method} ${req.originalUrl}`);
-//   next();
-// });
-
-// app.use(cors());
-// app.use(express.json());
-// app.set('supabaseAdmin', supabaseAdmin);
-// app.set('supabaseAnon', supabaseAnon);
-// app.use('/api/auth', authRoutes);
-// app.use('/api/polls', pollRoutes);
-// app.use('/api/questions', questionRoutes);
-// app.use('/api/ai', aiRoutes);
-// app.use(express.static(path.join(__dirname)));
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'index.html'));
-// });
-
-// app.use((req, res) => {
-//   res.status(404).json({ error: 'Route not found' });
-// });
-
-// app.use((err, req, res, next) => {
-//   console.error('Unhandled error:', err);
-//   res.status(500).json({ error: 'Internal server error' });
-// });
-
-// // app.listen(PORT, () => {
-// //   console.log(`Server running on port ${PORT}`);
-// // });//changed here
-
-// if (process.env.NODE_ENV !== 'production') {
-//   app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-//   });
-// }
-
-// module.exports = app;
-
-
-
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
@@ -90,6 +35,14 @@ app.get('/', (req, res) => {
 
 app.get('/dashboard.html', (req, res) => {
   res.sendFile(path.join(__dirname, 'dashboard.html'));
+});
+
+app.get('/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, 'style.css'));
+});
+
+app.get('/script.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'script.js'));
 });
 
 app.use((req, res) => {
